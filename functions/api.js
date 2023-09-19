b4553fd2dcb55a67f451e8f87ef7e1f25cb69d0b
dhiyo7 <dhireksa12@gmail.com>
2023-09-19 21:39:40 +0700

    [DHIYO][ADD] - cek function

---
+++ functions/api.js
@@ -0,0 +1,23 @@
+const PORT = 7777
+const express = require('express')
+var cors = require('cors');
+const morgan = require('morgan');
+const serverless = require("serverless-http")
+const router = require('../src/routes/api.route')
+
+const app = express()
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(morgan('dev'));
+app.use(cors())
+app.get('/', (req, res) => {
+    res.json(`🚀 server run on ${PORT}`)
+})
+
+app.use('/api', router)
+app.use(`/.netlify/functions/api`, router);
+
+app.listen(PORT, () => console.log(`RUN ON ${PORT}`))
+// Export the app and the serverless function
+module.exports = app;
+module.exports.handler = serverless(app);
--- src/api.js
+++ src/api.js
@@ -2,7 +2,8 @@ const PORT = 7777
 const express = require('express')
 var cors = require('cors');
 const morgan = require('morgan');
-
+const serverless = require("serverless-http")
+const router = require('./routes/api.route')
 
 const app = express()
 app.use(express.json());
@@ -13,5 +14,10 @@ app.get('/', (req, res) => {
     res.json(`🚀 server run on ${PORT}`)
 })
 
-app.use('/api', require('./src/routes/api.route'))
+app.use('/api', router)
+app.use(`/.netlify/functions/api`, router);
+
 app.listen(PORT, () => console.log(`RUN ON ${PORT}`))
+// Export the app and the serverless function
+module.exports = app;
+module.exports.handler = serverless(app);
